92a46f65551fdc69b39efd8bdabeece508eaf8d3
datasalih <salihsenova01@gmail.com>
2024-02-26 21:13:12 +0300

    1.9

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask,render_template, request, jsonify
 from werkzeug.utils import secure_filename
 import whisper
 import os
 
 app = Flask(__name__)
-
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'mp3', 'mp4', 'wav', 'm4a', 'flac'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -13,6 +12,7 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/')
 def home():
@@ -38,5 +38,8 @@ def transcribe_audio():
 
     return jsonify({'error': 'File type not allowed'}), 400
 
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
+
