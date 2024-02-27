b2c7ad41108ff1cf426c72b3285cff83152a507c
datasalih <salihsenova01@gmail.com>
2024-02-27 15:25:05 +0300

    3.0

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, jsonify, render_template
 from werkzeug.utils import secure_filename
-import whisper
+from google.cloud import speech
 import os
+
+project_id = os.environ.get('audio-to-text-415611')
+
+
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
@@ -33,13 +37,30 @@ def transcribe_audio():
         file.save(filepath)
 
         try:
-            model = whisper.load_model("tiny.en")  # Load model only when needed
-            result = model.transcribe(filepath)
-            return jsonify({'transcription': result['text']})
+            # Instantiates a client
+            client = speech.SpeechClient(project=project_id)
+
+            # Loads the audio into memory
+            with open(filepath, "rb") as audio_file:
+                content = audio_file.read()
+            audio = speech.RecognitionAudio(content=content)
+
+            config = speech.RecognitionConfig(
+                encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+                sample_rate_hertz=16000,
+                language_code="en-US",
+            )
+
+            # Detects speech in the audio file
+            response = client.recognize(config=config, audio=audio)
+
+            transcript = " ".join([result.alternatives[0].transcript for result in response.results])
+
+            return jsonify({'transcription': transcript})
         finally:
             os.remove(filepath)  
     else:
         return jsonify({'error': 'File type not allowed'}), 400
 
 if __name__ == "__main__":
-    app.run() 
+    app.run()
